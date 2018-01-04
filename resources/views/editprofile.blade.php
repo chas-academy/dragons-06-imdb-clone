4ecb3bc32d8d0d3f66afce6d855b794af2088f37
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-01-04 11:18:47 +0100

    fixed a profile picture change bug

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <img src="/uploads/profile/{{ $user->image }}" style="width:120px; border-radius:50%;"  alt="Profile picture">
+            <img src="/uploads/profile/{{ Auth::user()->image }}" style="width:120px; border-radius:50%;"  alt="Profile picture">
             <h3>{{ $user->name }}'s Profile. </h3>
             <form enctype="multipart/form-data" action="/editprofile" method="POST">
                 <label>Update Profile Picture</label><br>
