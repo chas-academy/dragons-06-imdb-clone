3df6e3dcdcc89595fee60abfea581ffe06e86ae4
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-01-04 11:34:26 +0100

    made the code more consistent

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <img src="/uploads/profile/{{ Auth::user()->image }}" style="width:120px; border-radius:50%;"  alt="Profile picture">
-            <h3>{{ $user->name }}'s Profile. </h3>
+            <img src="/uploads/profile/{{ Auth::user()->image }}" style="width:100px; border-radius:50%;"  alt="Profile picture">
+            <h3>Edit {{ Auth::user()->name }}'s Profile. </h3>
             <form enctype="multipart/form-data" action="/editprofile" method="POST">
                 <label>Update Profile Picture</label><br>
                 <input type="file" name="image">
@@ -17,9 +17,9 @@
             <br>
             <form enctype="multipart/form-data" action="/editprofile" method="POST">
                 <label>Update Profile</label><br>
-                <label>Bio: </label><textarea type="text" name="bio" rows="5" cols="35">{{ $user->bio }}</textarea>
+                <label>Bio: </label><textarea type="text" name="bio" rows="5" cols="35">{{ Auth::user()->bio }}</textarea>
                 <br>
-                <label>Name: </label><input type="text" name="name" value="{{ $user->name }}">
+                <label>Name: </label><input type="text" name="name" value="{{ Auth::user()->name }}">
                 <!-- hidden csrf token! for security -->
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <br>
