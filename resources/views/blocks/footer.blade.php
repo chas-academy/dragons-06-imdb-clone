f99df9b3f954163a7c7016016f626881bc45b8b4
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-01-07 21:43:08 +0100

    Initial structure and styling for footer. Changed background color for main to: #3C4858

--- resources/views/blocks/footer.blade.php
+++ resources/views/blocks/footer.blade.php
@@ -1,9 +1,47 @@
 <footer class="footer">
-<div class="container">
-<div class="content has-text-centered">
-  <p>
-    MoviKyte – movies high as hell!
-  </p>
-</div>
-</div>
+    <div class="container">
+
+        <nav class="level">
+            <div class="level-item has-text-centered">
+                <nav class="breadcrumb has-dot-separator">
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="level-item has-text-centered">
+                <span class="icon has-text-info">
+                            <i class="fa fa-facebook has-text-white"></i>
+                          </span>
+                <span class="icon has-text-success">
+                            <i class="fa fa-twitter has-text-white"></i>
+                          </span>
+                <span class="icon has-text-warning">
+                            <i class="fa fa-instagram has-text-white"></i>
+                          </span>
+                <span class="icon has-text-danger">
+                            <i class="fa fa-youtube has-text-white"></i>
+                          </span>
+            </div>
+            <div class="level-item has-text-centered has-text-white">
+                <label class="">Subscribe to our newsletter!</label>
+                <div class="field has-addons is-pulled-left">
+
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Enter your email">
+                    </div>
+                    <div class="control">
+                        <a class="button is-primary">
+                                    Subscribe
+                                  </a>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
 </footer>
+
+
+
+
