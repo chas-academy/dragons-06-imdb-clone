79734b269552d454e5b2fa5ae64a725be8ca54a6
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-01-15 14:45:08 +0100

    latest movies now works

--- resources/views/latestMovies.blade.php
+++ resources/views/latestMovies.blade.php
@@ -3,6 +3,18 @@
 Search Results
 @endsection
 @section('content')
+<section class="hero is-medium is-primary is-bold">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Primary bold title
+      </h1>
+      <h2 class="subtitle">
+        Primary bold subtitle
+      </h2>
+    </div>
+  </div>
+</section>
   <nav class="pagination is-centered" role="navigation" aria-label="pagination">
     <a class="pagination-previous">Previous</a>
     <a class="pagination-next">Next letter</a>
@@ -30,14 +42,59 @@ Search Results
       </li>
     </ul>
   </nav>
-  <div class="columns">
+  <div class="card">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-128x128">
+          <img src="https://bulma.io/images/placeholders/256x256.png" alt="Placeholder image">
+        </figure>
+      </div>
+      </div>
+      @foreach ($movies as $movie)
+      <div class="movie-title media-content">
+        <p class="title is-4">{{ $movie->title }}</p>
+        <p class="movie-actors subtitle is-6">{{ $movie->actors }}</p>
+      </div>
+
+    <div class="content">
+    {{ $movie->plot }}
+    </div>
+    @endforeach
+    <a class="button is-primary is-outlined">
+        <span class="icon is-small">
+          <i class="fa fa-thumbs-up"></i>
+        </span>
+        <h1 class="title is-6">Add to watchlist</h1>
+      </a>
+  </div>
+</div>
+  <!-- <div class="columns">
     <div class="column">
+  
+      <figure class="image is-128x128">
+        <img src="https://bulma.io/images/placeholders/128x128.png">
+      </figure>
+      <p class="title is-4">Title 1</p>
+      <h1 class="subtitle is-6">"Lorem t ducimus obcaecati delectus! Lorem ipsum dolor sit amet.</h1>
+      <a class="button is-primary is-outlined">
+        <span class="icon is-small">
+          <i class="fa fa-thumbs-up"></i>
+        </span>
+        <h1 class="title is-6">Add to watchlist</h1>
+      </a>
+    </div>
+  </div>
+   
+  
+  
+  <div class="column">
       Title
       <figure class="image is-128x128">
         <img src="https://bulma.io/images/placeholders/128x128.png">
       </figure>
-      <h1 class="title is-6">Lorem t ducimus obcaecati delectus! Lorem ipsum dolor sit amet.</h1>
-      <a class="button">
+      <h1 class="title is-6">Lorem ipsum, ne itaque sit ducimus obcaecati delectus!</h1>
+      <a class="button is-primary is-outlined">
         <span class="icon is-small">
           <i class="fa fa-thumbs-up"></i>
         </span>
@@ -50,20 +107,7 @@ Search Results
         <img src="https://bulma.io/images/placeholders/128x128.png">
       </figure>
       <h1 class="title is-6">Lorem ipsum, ne itaque sit ducimus obcaecati delectus!</h1>
-      <a class="button">
-        <span class="icon is-small">
-          <i class="fa fa-thumbs-up"></i>
-        </span>
-        <h1 class="title is-6">Add to watchlist</h1>
-      </a>
-    </div>
-    <div class="column">
-      Title
-      <figure class="image is-128x128">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </figure>
-      <h1 class="title is-6">Lorem ipsum, ne itaque sit ducimus obcaecati delectus!</h1>
-      <a class="button">
+      <a class="button is-primary is-outlined">
         <span class="icon is-small">
           <i class="fa fa-thumbs-up"></i>
         </span>
@@ -76,7 +120,7 @@ Search Results
           <img src="https://bulma.io/images/placeholders/128x128.png">
         </figure>
         <h1 class="title is-6">Lorem ipsum,sit ducimus obcaecati delectus!</h1>
-        <a class="button">
+        <a class="button is-primary is-outlined">
           <span class="icon is-small">
             <i class="fa fa-thumbs-up"></i>
           </span>
@@ -93,7 +137,7 @@ Search Results
         <img src="https://bulma.io/images/placeholders/128x128.png">
       </figure>
       <h1 class="title is-6">Lorem t ducimus obcaecati delectus Lorem ipsum dolor sit amet.!</h1>
-      <a class="button">
+      <a class="button is-primary is-outlined">
         <span class="icon is-small">
           <i class="fa fa-thumbs-up"></i>
         </span>
@@ -106,7 +150,7 @@ Search Results
         <img src="https://bulma.io/images/placeholders/128x128.png">
       </figure>
       <h1 class="title is-6">Lorem ipsum, ne itaque sit ducimus obcaecati delectus!</h1>
-      <a class="button">
+      <a class="button is-primary is-outlined">
         <span class="icon is-small">
           <i class="fa fa-thumbs-up"></i>
         </span>
@@ -119,7 +163,7 @@ Search Results
         <img src="https://bulma.io/images/placeholders/128x128.png">
       </figure>
       <h1 class="title is-6">Lorem ipsum, ne itaque sit ducimus obcaecati delectus!</h1>
-      <a class="button">
+      <a class="button is-primary is-outlined">
         <span class="icon is-small">
           <i class="fa fa-thumbs-up"></i>
         </span>
@@ -132,7 +176,7 @@ Search Results
           <img src="https://bulma.io/images/placeholders/128x128.png">
         </figure>
         <h1 class="title is-6">Lorem ipsum,sit ducimus obcaecati delectus!</h1>
-        <a class="button">
+        <a class="button is-primary is-outlined">
           <span class="icon is-small">
             <i class="fa fa-thumbs-up"></i>
           </span>
@@ -278,6 +322,6 @@ Search Results
       <li>
         <a class="pagination-link" aria-label="Goto page 86">86</a>
       </li>
-    </ul>
+    </ul> -->
   </nav>
 @endsection
