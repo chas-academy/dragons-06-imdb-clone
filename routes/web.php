0e85907aa7e4bcc4d2b8e96436bc42b228c7c399
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-01-19 14:47:58 +0100

    removed unused code

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::prefix('admin')->group(function() {
 	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 
-	Route::get('/editusers', 'UserController@editusersform')->name('admin.dashboard');
+	Route::get('/editusers', 'UserController@editusersform');
 	Route::post('/editusers', 'UserController@edit');
 
 	Route::get('/create', 'MovieController@create');
