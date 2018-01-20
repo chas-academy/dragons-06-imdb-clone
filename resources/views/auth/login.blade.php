f47b3066b1b1946a3e2455d23c77b029184dd138
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-01-20 15:30:09 +0100

    styling for loginpage done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
-<section class="hero is-medium is-primary is-bold">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Log in
-      </h1>
-      <h2 class="subtitle">
-        Enter MovieKyte!
-      </h2>
-    </div>
-  </div>
-</section>
-<div class="container margin">
+<div class="register-background-image">
+<!-- <section class="hero is-medium is-primary is-bold"> -->
+<section class="register-form">
+    <h1 class="title has-text-light">Sign In</h1>
+
+<!-- </section> -->
+<!-- <div class="container margin"> -->
+
     @include('blocks.login')
 </div>
+</section>
 @endsection
