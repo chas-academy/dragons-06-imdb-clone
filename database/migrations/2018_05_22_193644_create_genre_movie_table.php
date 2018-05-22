6e8300c06fd55b503854d1872121933403c64c6f
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-05-22 21:49:57 +0200

    Create a genres table and a genre movie table with relations as requested.

---
+++ database/migrations/2018_05_22_193644_create_genre_movie_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGenreMovieTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genre_movie', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('genre_id')->unsigned();
+            $table->integer('movie_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('genre_id')->references('genres')->on('id')->onDelete('cascade');
+            $table->foreign('movie_id')->references('movies')->on('id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genre_movie');
+    }
+}
