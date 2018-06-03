a7ee279d3c79b441a81338128330a8052cbf060d
martinfjant <martin@falkjohansson.se>
2018-06-03 22:31:51 +0200

    added the migrations

---
+++ database/migrations/2018_06_03_195420_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('city')->nullable();
+            $table->string('profile_pic')->nullable();
+            $table->enum('type', ['actor', 'director', 'producer']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
